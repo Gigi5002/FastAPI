8031a26f26294b57851259bb35492a2da60af175
Gulmira <gulmiraisakbaeva78@gmail.com>
2026-05-06 21:56:40 +0600

    Статические файлы

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
-import mimetypes
-from fastapi import FastAPI, Response, Path
-from fastapi.responses import RedirectResponse, PlainTextResponse
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
  
 app = FastAPI()
- 
-@app.get("/users/{id}", status_code=200)
-def users(response: Response, id: int = Path()):
-    if id < 1:
-        response.status_code = 400
-        return {"message": "Incorrect Data"}
-    return  {"message": f"Id = {id}"}
+app.mount("/static", StaticFiles(directory="public"))
 
 
-@app.get("/old")
-def old():
-    return RedirectResponse("/new")
- 
-@app.get("/new")
-def new():
-    return PlainTextResponse("Новая страница")
