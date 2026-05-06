9b89c1a5be9b5abb5b5177ceadefbe11d15ad3c6
Gulmira <gulmiraisakbaeva78@gmail.com>
2026-05-06 19:48:15 +0600

    Параметры пути

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import mimetypes
-from fastapi import FastAPI
-from fastapi.responses import FileResponse
+from fastapi import FastAPI, Path
  
 app = FastAPI()
  
-@app.get("/")
-def root():
-    return FileResponse("public/index.html", 
-                        filename="mainpage.html", 
-                        media_type="application/octet-stream")
+@app.get("/users/{name}/{age}")
+def users(name:str  = Path(min_length=3, max_length=20), 
+            age: int = Path(ge=18, lt=111)):
+    return {"name": name, "age": age}
