08e9f935d769a85b35d5333b82658620fbb54a6c
Gulmira <gulmiraisakbaeva78@gmail.com>
2026-05-07 17:59:38 +0600

    Куки

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI, Header
+from fastapi import FastAPI, Response, Cookie
+from datetime import datetime
  
 app = FastAPI()
  
- 
 @app.get("/")
-def root(user_agent: str = Header()):
-    return {"User-Agent": user_agent}
+def root(response: Response):
+    now = datetime.now()    # получаем текущую дату и время
+    response.set_cookie(key="last_visit", value=now)
+    return  {"message": "куки установлены"}
+
+@app.get("/visit")
+def root(last_visit: str | None = Cookie(default=None)):
+    if last_visit == None:
+        return {"message": "Это ваш первый визит на сайт"}
+    else:
+        return  {"message": f"Ваш последний визит: {last_visit}"}
