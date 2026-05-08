117c482529c4a6ec0c8c7731160acac8458dc3e2
Gulmira <gulmiraisakbaeva78@gmail.com>
2026-05-08 16:47:53 +0600

    Курсов валют

--- main.py
+++ main.py
@@ -1,74 +1,19 @@
-from database import *
-from sqlalchemy.orm import Session
-from fastapi import Depends, FastAPI, Body
-from fastapi.responses import JSONResponse, FileResponse
- 
-# создаем таблицы
-Base.metadata.create_all(bind=engine)
- 
+from fastapi import FastAPI
+import requests
+
 app = FastAPI()
- 
-# определяем зависимость
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-  
-@app.get("/")
-def main():
-    return FileResponse("public/index.html")
-  
-@app.get("/api/users")
-def get_people(db: Session = Depends(get_db)):
-    return db.query(Person).all()
-  
-@app.get("/api/users/{id}")
-def get_person(id, db: Session = Depends(get_db)):
-    # получаем пользователя по id
-    person = db.query(Person).filter(Person.id == id).first()
-    # если не найден, отправляем статусный код и сообщение об ошибке
-    if person==None:  
-        return JSONResponse(status_code=404, content={ "message": "Пользователь не найден"})
-    #если пользователь найден, отправляем его
-    return person
-  
-  
-@app.post("/api/users")
-def create_person(data  = Body(), db: Session = Depends(get_db)):
-    person = Person(name=data["name"], age=data["age"])
-    db.add(person)
-    db.commit()
-    db.refresh(person)
-    return person
-  
-@app.put("/api/users")
-def edit_person(data  = Body(), db: Session = Depends(get_db)):
-   
-    # получаем пользователя по id
-    person = db.query(Person).filter(Person.id == data["id"]).first()
-    # если не найден, отправляем статусный код и сообщение об ошибке
-    if person == None: 
-        return JSONResponse(status_code=404, content={ "message": "Пользователь не найден"})
-    # если пользователь найден, изменяем его данные и отправляем обратно клиенту
-    person.age = data["age"]
-    person.name = data["name"]
-    db.commit() # сохраняем изменения 
-    db.refresh(person)
-    return person
-  
-  
-@app.delete("/api/users/{id}")
-def delete_person(id, db: Session = Depends(get_db)):
-    # получаем пользователя по id
-    person = db.query(Person).filter(Person.id == id).first()
-   
-    # если не найден, отправляем статусный код и сообщение об ошибке
-    if person == None:
-        return JSONResponse( status_code=404, content={ "message": "Пользователь не найден"})
-   
-    # если пользователь найден, удаляем его
-    db.delete(person)  # удаляем объект
-    db.commit()     # сохраняем изменения
-    return person
+
+@app.get("/api/kurs/{currency_code}")
+def get_currency_rate(currency_code: str):
+    url = f"https://api.exchangerate-api.com/v4/latest/{currency_code.upper()}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        return {
+            "base_currency": data["base"],
+            "date": data["date"],
+            "rates": data["rates"]
+        }
+    else:
+        return {"error": "Курсы валют в данное время недоступны"}
