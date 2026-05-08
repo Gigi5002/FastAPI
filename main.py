919b815a8deda469826aed58b2cfa664e5bb12a0
Gulmira <gulmiraisakbaeva78@gmail.com>
2026-05-08 13:29:03 +0600

    Базовые операции с базой данных в веб-приложении

--- main.py
+++ main.py
@@ -1,29 +1,74 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import  Column, Integer, String
+from database import *
+from sqlalchemy.orm import Session
+from fastapi import Depends, FastAPI, Body
+from fastapi.responses import JSONResponse, FileResponse
  
-from fastapi import FastAPI
+# создаем таблицы
+Base.metadata.create_all(bind=engine)
  
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
- 
- 
-class Base(DeclarativeBase): pass
-class Person(Base):
-    __tablename__ = "people"
- 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    age = Column(Integer,)
- 
-SessionLocal = sessionmaker(autoflush=False, bind=engine)
-db = SessionLocal()
- 
-# получение всех объектов
-people = db.query(Person).all()
-for p in people:
-    print(f"{p.id}.{p.name} ({p.age})")
- 
-# приложение, которое ничего не делает
 app = FastAPI()
+ 
+# определяем зависимость
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+  
+@app.get("/")
+def main():
+    return FileResponse("public/index.html")
+  
+@app.get("/api/users")
+def get_people(db: Session = Depends(get_db)):
+    return db.query(Person).all()
+  
+@app.get("/api/users/{id}")
+def get_person(id, db: Session = Depends(get_db)):
+    # получаем пользователя по id
+    person = db.query(Person).filter(Person.id == id).first()
+    # если не найден, отправляем статусный код и сообщение об ошибке
+    if person==None:  
+        return JSONResponse(status_code=404, content={ "message": "Пользователь не найден"})
+    #если пользователь найден, отправляем его
+    return person
+  
+  
+@app.post("/api/users")
+def create_person(data  = Body(), db: Session = Depends(get_db)):
+    person = Person(name=data["name"], age=data["age"])
+    db.add(person)
+    db.commit()
+    db.refresh(person)
+    return person
+  
+@app.put("/api/users")
+def edit_person(data  = Body(), db: Session = Depends(get_db)):
+   
+    # получаем пользователя по id
+    person = db.query(Person).filter(Person.id == data["id"]).first()
+    # если не найден, отправляем статусный код и сообщение об ошибке
+    if person == None: 
+        return JSONResponse(status_code=404, content={ "message": "Пользователь не найден"})
+    # если пользователь найден, изменяем его данные и отправляем обратно клиенту
+    person.age = data["age"]
+    person.name = data["name"]
+    db.commit() # сохраняем изменения 
+    db.refresh(person)
+    return person
+  
+  
+@app.delete("/api/users/{id}")
+def delete_person(id, db: Session = Depends(get_db)):
+    # получаем пользователя по id
+    person = db.query(Person).filter(Person.id == id).first()
+   
+    # если не найден, отправляем статусный код и сообщение об ошибке
+    if person == None:
+        return JSONResponse( status_code=404, content={ "message": "Пользователь не найден"})
+   
+    # если пользователь найден, удаляем его
+    db.delete(person)  # удаляем объект
+    db.commit()     # сохраняем изменения
+    return person
